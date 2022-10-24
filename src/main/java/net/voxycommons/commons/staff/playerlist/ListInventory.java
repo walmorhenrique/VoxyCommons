8b3fef2cc5befb9bda41c191f5b3a39afd2b9c61
Thales Marinho <thalesmarinhotwn@hotmail.com>
2022-10-23 23:09:46 -0300

    Viajei

--- src/main/java/net/voxycommons/commons/staff/playerlist/ListInventory.java
+++ src/main/java/net/voxycommons/commons/staff/playerlist/ListInventory.java
@@ -36,15 +36,15 @@ public class ListInventory extends PagedInventory {
         ItemStack backStack = back.getItemStack();
         ItemMeta meta = backStack.getItemMeta();
         meta.setDisplayName(YmlConfigurator.MStaff.getString("Menus.Menu_PlayerList.itens.voltar pagina.nome"));
-        meta.setLore(YmlConfigurator.MStaff.getStringList("Menus.Menu_PlayerList.itens.voltar pagina.lore").stream().map(lore -> lore.replace("&", "�")).collect(Collectors.toList()));
+        meta.setLore(YmlConfigurator.MStaff.getStringList("Menus.Menu_PlayerList.itens.voltar pagina.lore").stream().map(lore -> lore.replace("&", "§")).collect(Collectors.toList()));
         backStack.setItemMeta(meta);
         InventoryItem next = DefaultItem.NEXT_PAGE.toInventoryItem(viewer);
         ItemStack nextItemStack = next.getItemStack();
         ItemMeta meta2 = nextItemStack.getItemMeta();
         meta2.setDisplayName(YmlConfigurator.MStaff.getString("Menus.Menu_PlayerList.itens.proxima pagina.nome"));
-        meta2.setLore(YmlConfigurator.MStaff.getStringList("Menus.Menu_PlayerList.itens.proxima pagina.lore").stream().map(lore -> lore.replace("&", "�")).collect(Collectors.toList()));
+        meta2.setLore(YmlConfigurator.MStaff.getStringList("Menus.Menu_PlayerList.itens.proxima pagina.lore").stream().map(lore -> lore.replace("&", "§")).collect(Collectors.toList()));
         nextItemStack.setItemMeta(meta2);
-        ItemStack voltar = new ItemBuilder(Material.getMaterial(YmlConfigurator.MStaff.getString("Menus.Menu_PlayerList.itens.voltar-menu.material")), 1, (short)YmlConfigurator.MStaff.getInt("Menus.Menu_PlayerList.itens.voltar-menu.data")).setName(YmlConfigurator.MStaff.getString("Menus.Menu_PlayerList.itens.voltar-menu.nome").replace("&", "�")).setLore((List<String>)YmlConfigurator.MStaff.getStringList("Menus.Menu_PlayerList.itens.voltar-menu.lore").stream().map(lore -> lore.replace("&", "�")).collect(Collectors.toList())).build();
+        ItemStack voltar = new ItemBuilder(Material.getMaterial(YmlConfigurator.MStaff.getString("Menus.Menu_PlayerList.itens.voltar-menu.material")), 1, (short)YmlConfigurator.MStaff.getInt("Menus.Menu_PlayerList.itens.voltar-menu.data")).setName(YmlConfigurator.MStaff.getString("Menus.Menu_PlayerList.itens.voltar-menu.nome").replace("&", "§")).setLore((List<String>)YmlConfigurator.MStaff.getStringList("Menus.Menu_PlayerList.itens.voltar-menu.lore").stream().map(lore -> lore.replace("&", "§")).collect(Collectors.toList())).build();
         editor.setItem(YmlConfigurator.MStaff.getInt("Menus.Menu_PlayerList.itens.voltar-menu.slot"), InventoryItem.of(voltar));
     }
     
@@ -52,7 +52,7 @@ public class ListInventory extends PagedInventory {
     protected List<InventoryItemSupplier> createPageItems(PagedViewer viewer) {
         List<InventoryItemSupplier> itemSuppliers = new LinkedList<>();
         for (Player player : Bukkit.getOnlinePlayers()) {
-            ItemStack skull = new SkullBuilder(player.getName(), false).setDisplayName(YmlConfigurator.MStaff.getString("Menus.Menu_PlayerList.itens.format.nome").replace("&", "�").replace("{jogador}", player.getName())).setLore((List<String>)YmlConfigurator.MStaff.getStringList("Menus.Menu_PlayerList.itens.format.lore").stream().map(lore -> lore.replace("&", "�")).collect(Collectors.toList())).build();
+            ItemStack skull = new SkullBuilder(player.getName(), false).setDisplayName(YmlConfigurator.MStaff.getString("Menus.Menu_PlayerList.itens.format.nome").replace("&", "§").replace("{jogador}", player.getName())).setLore((List<String>)YmlConfigurator.MStaff.getStringList("Menus.Menu_PlayerList.itens.format.lore").stream().map(lore -> lore.replace("&", "§")).collect(Collectors.toList())).build();
             itemSuppliers.add(() -> InventoryItem.of(skull));
         }
         return itemSuppliers;
--- src/main/java/net/voxycommons/utils/YmlConfigurator.java
+++ src/main/java/net/voxycommons/utils/YmlConfigurator.java
@@ -5,11 +5,12 @@ import net.voxycommons.*;
 
 public class YmlConfigurator {
 
-    public static FileConfiguration Cconfig = Main.Config.getConfig();
-    public static FileConfiguration CStaff = Main.ConfigStaff.getConfig();
-    public static FileConfiguration CCuras = Main.ConfigCuras.getConfig();
-    public static FileConfiguration MStaff = Main.MenusStaff.getConfig();
-    public static FileConfiguration CProfilerAnimations = Main.ConfigProfilerAnimations.getConfig();
-    public static FileConfiguration MProfilerAnimations = Main.MenusProfilerAnimations.getConfig();
-    public static FileConfiguration CEssentials = Main.ConfigEssentials.getConfig();
+    public static FileConfiguration Cconfig = Main.Config.get();
+    public static FileConfiguration CStaff = Main.ConfigStaff.get();
+    public static FileConfiguration CCuras = Main.ConfigCuras.get();
+    public static FileConfiguration MStaff = Main.MenusStaff.get();
+    public static FileConfiguration CProfilerAnimations = Main.ConfigProfilerAnimations.get();
+    public static FileConfiguration MProfilerAnimations = Main.MenusProfilerAnimations.get();
+    public static FileConfiguration CEssentials = Main.ConfigEssentials.get();
+
 }
